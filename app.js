4bc627b7bf9b890e8c9297c802d1bdb57d8cec87
Microsmsm <drusama2@gmail.com>
2017-11-17 21:16:57 +0200

    edit get

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api/isSadPost', (req, res) => res.send('post some thing as a text parameter to me'))
 
 app.post('/api/isSadPost', (req, res) => {
     if (req.body.text) {
